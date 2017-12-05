5a037ede874429b04f3d8075420317a4cb50cfda
Ricardo Brazileiro <rbrazileiro@gmail.com>
2017-12-05 17:54:22 -0300

    layout frontend

--- html_template/assets/js/main.js
+++ html_template/assets/js/main.js
@@ -4,35 +4,34 @@
 $(document).ready(function(){
   'use strict';
 
-  window.sr = ScrollReveal({ reset: true, mobile: false });
+  window.sr = ScrollReveal({ reset: false, mobile: false });
 
   var topLight = {
     origin   : "top",
-    distance : "2rem",
+    distance : "0.5em",
     duration : 1500,
     scale    : 3.05,
   }
   var rightLight = {
     origin   : "right",
-    distance : "2rem",
-    duration : 2000,
-    scale    : 5.05,
+    distance : "0.5em",
+    duration : 1600,
+    scale    : 3.05,
   }
 
-  var intro = {
-    origin   : "bottom",
-    distance : "64px",
-    duration : 900,
-    delay    : 1500,
+  var topLight = {
+    origin   : "top",
+    distance : "0em",
+    duration : 1000,
+    delay    : 1000,
     scale    : 1,
   }
-
-  var github = {
-    origin   : "top",
-    distance : "32px",
-    duration : 600,
-    delay    : 1800,
-    scale    : 0,
+  var background = {
+    origin   : "bottom",
+    distance : "0em",
+    duration : 300,
+    delay    : 100,
+    scale    : 1,
   }
 
   var block = {
@@ -42,5 +41,11 @@ $(document).ready(function(){
 
   sr.reveal(".s01, .s02, .s03", topLight)
   sr.reveal(".s04, .s05", rightLight)
+  sr.reveal(".scene-background-purple", background)
+  sr.reveal(".scene-city", topLight)
+  sr.reveal(".scene-entrepreneur", topLight)
+  sr.reveal(".scene-genetic", topLight)
+  sr.reveal(".scene-world", topLight)
+
 
 });
