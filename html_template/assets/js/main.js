e7ca03dea0b78bb986ef45d6dfd22a2a241e2aa6
Ricardo Brazileiro <rbrazileiro@gmail.com>
2017-12-13 17:30:06 -0300

    mobile first

--- html_template/assets/js/main.js
+++ html_template/assets/js/main.js
@@ -6,24 +6,31 @@ $(document).ready(function(){
 
   window.sr = ScrollReveal({ reset: false, mobile: true });
 
-  var topLight = {
-    origin   : "top",
-    distance : "0.1em",
-    duration : 200,
-    scale    : 3.05,
-  }
   var rightLight = {
     origin   : "right",
     distance : "0.5em",
     duration : 200,
     scale    : 3.05,
   }
-
   var topLight = {
     origin   : "top",
     distance : "0em",
     duration : 1000,
     delay    : 1000,
+    scale    : 1,
+  }
+  var topLightSlow = {
+    origin   : "top",
+    distance : "0em",
+    duration : 300,
+    delay    : 1300,
+    scale    : 1,
+  }
+  var topLightVerySlow = {
+    origin   : "top",
+    distance : "0em",
+    duration : 300,
+    delay    : 2000,
     scale    : 1,
   }
   var background = {
@@ -40,12 +47,14 @@ $(document).ready(function(){
   }
 
   sr.reveal(".s01, .s02, .s03", topLight)
-  sr.reveal(".s04, .s05", rightLight)
   sr.reveal(".scene-background-purple", background)
   sr.reveal(".scene-city", topLight)
-  sr.reveal(".scene-entrepreneur", topLight)
-  sr.reveal(".scene-genetic", topLight)
+  sr.reveal(".scene-entrepreneur", topLightSlow)
+  sr.reveal(".scene-text-reparte-1", topLight)
   sr.reveal(".scene-world", topLight)
+  sr.reveal(".scene-world-trace-1, .scene-world-trace-2, .scene-world-trace-3", topLightVerySlow)
+  sr.reveal(".scene-text-reparte-2", topLight)
+
 
 
 });
