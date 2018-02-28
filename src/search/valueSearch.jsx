94f04881abd0aa686190e1616c67ea73da3498d0
mrov <mrov@cin.ufpe.br>
2018-02-28 16:53:02 -0300

    Estilo da navbar ajustado, tela de busca por valor

--- src/search/valueSearch.jsx
+++ src/search/valueSearch.jsx
@@ -51,32 +51,32 @@ export default class Login extends Component {
         <Header logado={extraFunctions.checklogin(this.state.token)} history={this.props.history}/>
 			  {extraFunctions.redirectNotLoged(this.state.token)}
         <div className="wrapper">
-			  <div id="search-div" className="page-header value-search-background">
-				<div className="filter"></div>
-				<div className="container">
-					<div className="row">
-						<div className="col-12 col-md-6 ml-auto mr-auto">
-							<div className="card card-plain card-search ml-auto mr-auto p-0">
-								<h3>Valor a ser repartido:</h3>
-								<form className="register-form">
-									<div className="input-group mt-3 mb-2">
-										<span className="input-group-addon"><i className="mr-1 fa fa-search"></i></span>
-                    <input type="number" placeholder="US$ 810.000,00" className="form-control pull-right" onChange={(event) => {this.setState({value: event.target.value})}} required/>
+          <div id="search-div" className="page-header value-search-background">
+            <div className="filter"></div>
+            <div className="container">
+              <div className="row">
+                <div className="col-12 col-md-6 ml-auto mr-auto">
+                  <div className="card card-plain card-search ml-auto mr-auto p-0">
+                    <h3>Valor a ser repartido:</h3>
+                    <form className="register-form">
+                      <div className="input-group mt-3 mb-2">
+                        <span className="input-group-addon"><i className="mr-1 fa fa-search"></i></span>
+                        <input type="number" placeholder="US$ 810.000,00" className="form-control pull-right" onChange={(event) => {this.setState({value: event.target.value})}} required/>
+                      </div>
+                      <small className={this.state.message.class}>{this.state.message.data}</small>
+                      <small className="ml-2">Digite apenas numeros*</small>
+                      <div className="col-6 col-sm-6 col-md-6 ml-auto mr-auto z-1">
+                        <div className="ard card-plain card-search p-0 ml-auto mr-auto search-btn">
+                          <button onClick={(event) => {this.submitHandle(event)}} className="btn btn-danger btn-block btn-round">Buscar</button>
+                        </div>
+                      </div>
+                    </form>
                   </div>
-                  <small className={this.state.message.class}>{this.state.message.data}</small>
-                  <small className="ml-2">Digite apenas numeros*</small>
-                  <div className="col-6 col-sm-6 col-md-6 ml-auto mr-auto z-1">
-                    <div className="ard card-plain card-search p-0 ml-auto mr-auto search-btn">
-                      <button onClick={(event) => {this.submitHandle(event)}} className="btn btn-danger btn-block btn-round">Buscar</button>
-                    </div>
-                  </div>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
+                </div>
+              </div>
             </div>
+          </div>
+        </div>
       </div>
     );
   }
